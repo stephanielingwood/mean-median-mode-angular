8a546b380261fb185ccee0fccff27adddd679e26
Stephanie Lingwood <stephanielingwood@gmail.com>
2014-12-06 22:43:31 -0800

    now testing client-side, not server-side, mmm logic

--- test/client/client_test.js
+++ test/client/client_test.js
@@ -5,7 +5,6 @@ require('angular-mocks');
 
 describe('mmmController', function() {
   var $controllerConstructor;
-  var $httpBackend;
   var $scope;
 
   beforeEach(angular.mock.module('mmmApp'));
@@ -20,4 +19,31 @@ describe('mmmController', function() {
     expect(typeof mmmController).toBe('object');
   });
 
+  describe('finding mean, median, and mode', function() {
+
+    it('should find the mean', function() {
+      $controllerConstructor('mmmCtrl', {$scope: $scope});
+      $scope.mmm = {};
+      $scope.mmm.meanNums = '3,5,5,7';
+      $scope.findMean();
+      expect($scope.mean).toBe(5);
+    });
+
+    it('should find the median', function() {
+      $controllerConstructor('mmmCtrl', {$scope: $scope});
+      $scope.mmm = {};
+      $scope.mmm.medianNums = '3,5,5,7';
+      $scope.findMedian();
+      expect($scope.median).toBe(5);
+    });
+
+    it('should find the mode', function() {
+      $controllerConstructor('mmmCtrl', {$scope: $scope});
+      $scope.mmm = {};
+      $scope.mmm.modeNums = '3,5,5,7';
+      $scope.findMode();
+      expect($scope.mode).toBe(5);
+    });
+
+  });
 });
